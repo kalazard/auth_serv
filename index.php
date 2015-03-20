9862674f70ea846e10bedc7b67b04171414947c1
root <root@workstation>
2015-03-20 21:31:53 +0100

    Web services qui ne plante pas

--- index.php
+++ index.php
@@ -4,34 +4,27 @@
  * pour rendre notre Service disponible
  *
  */
-
-//Cette classe pourra contenir d'autres méthodes accessibles via le SoapServer
-class LoginServer{
-	
-	//fonction qui test si l'utilisateur en argument existe
-	function CheckLogin()
-	{
-
-	}
+class Utilisateur
+{
+  function get($i)
+  {
+    return $i;
+  }
+  function hello()
+  {
+    return "hello world";
+  }
 }
 
-//Cette option du fichier php.ini permet de ne pas stocker en cache le fichier WSDL, afin de pouvoir faire nos tests
-//Car le cache se renouvelle toutes les 24 heures, ce qui n'est pas idéal pour le développement
-ini_set('soap.wsdl_cache_enabled', 0);
+try
+{
+  $server = new SoapServer(null, array('uri' => 'http://localhost/auth_serv/index.php'));
 
-//L'instanciation du SoapServer se déroule de la même manière que pour le client : voir la doc pour plus d'informations sur les 
-//Différentes options disponibles 
-$serversoap=new SoapServer("http://130.79.214.167/auth_serv/wsdl/serv.wsdl");
-
-//Ici nous déclarons la classe qui sera servie par le Serveur SOAP, c'est cette déclaration qui fera le coeur de notre Servie WEB.
-//Je déclare que je sers une classe contenant des méthodes accessibles, on peut aussi déclarer plus simplement des fonctions
-//par l'instruction addFunction() : $serversoap->addFunction("retourDate"); à ce moment-là nous ne faisons pas de classe.
-
-//Noter le style employé pour la déclaration : le nom de la classe est passé en argument de type String, et non pas de variable...
-$serversoap->setClass("LoginServer");
-
-
-//Ici, on dit très simplement que maintenant c'est à PHP de prendre la main pour servir le Service WEB : il s'occupera de l'encodage XML, des
-//Enveloppes SOAP, de gérer les demandes clientes, etc. Bref, on en a fini avec le serveur SOAP !!!!
-$serversoap->handle();
+  $server->setClass("Utilisateur");
+  $server->handle();
+}
+catch(Exception $e)
+{
+  echo "Exception: " . $e;
+}
 
